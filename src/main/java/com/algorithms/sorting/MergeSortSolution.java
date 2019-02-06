802ee1596c68927a45c122da1556f9a5ecd95163
ozan aksoy <rockcoden@gmail.com>
2019-02-06 18:09:49 +0300

    [ADD]: Merge Sort Solution - placeholder

---
+++ src/main/java/com/algorithms/sorting/MergeSortSolution.java
@@ -0,0 +1,11 @@
+package com.algorithms.sorting;
+
+public class MergeSortSolution implements SortSolution{
+
+	@Override
+	public int[] sort(int[] inputArray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
