99f471d14464bf6aba474b45e2703ab48544e388
ozan aksoy <rockcoden@gmail.com>
2019-02-08 11:56:35 +0300

    [UPDATE]: Mergesort types updated to include new types of naive sorters

--- src/main/java/com/algorithms/sorting/SortSolutionFactory.java
+++ src/main/java/com/algorithms/sorting/SortSolutionFactory.java
@@ -33,8 +33,14 @@ public class SortSolutionFactory {
 			return new InsertionSortSolution();
 		case SELECTION:
 			return new SelectionSortSolution();
-		case MERGE_RECURSIVE_NAIVE:
-			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE).getActiveSolution();
+		case MERGE_RECURSIVE_NAIVE_SORTER:
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_SORTER).getActiveSolution();
+		case MERGE_RECURSIVE_NLOGN_SPACE:
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_NLOGN_SPACE).getActiveSolution();
+		case MERGE_RECURSIVE_N_SPACE:
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_N_SPACE).getActiveSolution();
+		case MERGE_RECURSIVE_ONE_SPACE:
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_ONE_SPACE).getActiveSolution();
 		case MERGE_RECURSIVE_POLISHED:
 			return new MergeSortSolution(MergeSortVersion.RECURSIVE_POLISHED).getActiveSolution();
 		case MERGE_RECURSIVE_ARRAY_COPY:
