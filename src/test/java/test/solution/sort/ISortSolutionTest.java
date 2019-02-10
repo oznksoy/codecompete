781687a244ac5795eb2310958dd36b2c404c9451
ozan aksoy <rockcoden@gmail.com>
2019-02-10 11:19:34 +0300

    [ADD]: Sort Solution Tests - Common Interface

---
+++ src/test/java/test/solution/sort/ISortSolutionTest.java
@@ -0,0 +1,19 @@
+package test.solution.sort;
+
+interface ISortSolutionTest {
+
+	public void testRandomizedTestCase();
+
+	public void testBestCase();
+
+	public void testWorstCase();
+
+	public void testHalfInOrderOnMedian();
+
+	public void testHalfInOrderOnMedianInReverse();
+
+	public void testHalfInOrderatRandom();
+
+	public void testOneToFiveCase();
+
+}
