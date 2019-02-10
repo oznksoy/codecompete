7f0ee0b911045e407fe09fc322c73821dfe2e911
ozan aksoy <rockcoden@gmail.com>
2019-02-10 11:20:39 +0300

    [UPDATE]: new sorting solution selections

--- src/main/java/com/algorithms/sorting/SortSolutionFactory.java
+++ src/main/java/com/algorithms/sorting/SortSolutionFactory.java
@@ -21,28 +21,28 @@ public class SortSolutionFactory {
 	public SortSolution getSolution(SortSolutionType type) throws SortSolutionSelectionException,
 			BubbleSortSolutionSelectionException, MergeSortSolutionSelectionException {
 		switch (type) {
-		case PLAIN:
+		case BRUTE:
 			return new PlainSortSolution();
-		case BUBBLE_V1:
-			return new BubbleSortSolution(BubbleSortVersion.V1).getActiveSolution();
-		case BUBBLE_V2:
-			return new BubbleSortSolution(BubbleSortVersion.V2).getActiveSolution();
-		case BUBBLE_V3:
-			return new BubbleSortSolution(BubbleSortVersion.V3).getActiveSolution();
+		case BUBBLE_UNCHECKED_SWAPS:
+			return new BubbleSortSolution(BubbleSortVersion.UNCHECKED_SWAPS).getActiveSolution();
+		case BUBBLE_IS_SWAP_CHECKED:
+			return new BubbleSortSolution(BubbleSortVersion.IS_SWAP_CHECKED).getActiveSolution();
+		case BUBBLE_LAST_SWAP_INDEX_CHECK:
+			return new BubbleSortSolution(BubbleSortVersion.LAST_SWAP_INDEX_CHECK).getActiveSolution();
 		case INSERTION:
 			return new InsertionSortSolution();
 		case SELECTION:
 			return new SelectionSortSolution();
-		case MERGE_RECURSIVE_NAIVE_SORTER:
-			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_SORTER).getActiveSolution();
+		case MERGE_RECURSIVE_SORTED_BY_INSERTION:
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_SORTED_BY_INSERTION).getActiveSolution();
 		case MERGE_RECURSIVE_NLOGN_SPACE:
-			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_NLOGN_SPACE).getActiveSolution();
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NLOGN_SPACE).getActiveSolution();
 		case MERGE_RECURSIVE_N_SPACE:
-			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_N_SPACE).getActiveSolution();
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_N_SPACE).getActiveSolution();
 		case MERGE_RECURSIVE_ONE_SPACE:
-			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NAIVE_ONE_SPACE).getActiveSolution();
-		case MERGE_RECURSIVE_POLISHED:
-			return new MergeSortSolution(MergeSortVersion.RECURSIVE_POLISHED).getActiveSolution();
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_ONE_SPACE).getActiveSolution();
+		case MERGE_RECURSIVE_NLOGN_SPACE_DIVISION_ON_MERGE:
+			return new MergeSortSolution(MergeSortVersion.RECURSIVE_NLOGN_SPACE_DIVISION_ON_MERGE).getActiveSolution();
 		case MERGE_RECURSIVE_ARRAY_COPY:
 			return new MergeSortSolution(MergeSortVersion.RECURSIVE_ARRAY_COPY).getActiveSolution();
 		case MERGE_NON_RECURSIVE:
