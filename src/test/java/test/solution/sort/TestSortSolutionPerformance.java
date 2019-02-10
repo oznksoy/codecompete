ae5dda1aaa7e701e970a360fc3389bde9dc24f34
ozan aksoy <rockcoden@gmail.com>
2019-02-10 11:24:44 +0300

    [UPDATE]: Mass random test case is updated to performance test implementation

---
+++ src/test/java/test/solution/sort/TestSortSolutionPerformance.java
@@ -0,0 +1,82 @@
+package test.solution.sort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.algorithms.sorting.SortSolutionType;
+
+public class TestSortSolutionPerformance {
+
+	SortingTestRequest testRequest;
+	SortingTestResponse testResponse;
+	SortingTestSupport testSupport;
+
+	@Before
+	public void setup() {
+
+		this.testSupport = new SortingTestSupport();
+		this.testRequest = testSupport.createTestRequest(50000, 50000, 0);
+		this.testResponse = this.testSupport.prepareTest(testRequest);
+	}
+
+	@Test
+	public void testBruteSort() {
+		testSupport.runTestCase(testResponse, SortSolutionType.BRUTE);
+	}
+
+	@Test
+	public void testBubbleSortUncheckedSwaps() {
+		testSupport.runTestCase(testResponse, SortSolutionType.BUBBLE_UNCHECKED_SWAPS);
+	}
+
+	@Test
+	public void testBubbleSortIsSwapChecked() {
+		testSupport.runTestCase(testResponse, SortSolutionType.BUBBLE_IS_SWAP_CHECKED);
+	}
+
+	@Test
+	public void testBubbleSortLastSwapIndexCheck() {
+		testSupport.runTestCase(testResponse, SortSolutionType.BUBBLE_LAST_SWAP_INDEX_CHECK);
+	}
+
+	@Test
+	public void testSelectionSort() {
+		testSupport.runTestCase(testResponse, SortSolutionType.SELECTION);
+	}
+
+	@Test
+	public void testInsersionSort() {
+		testSupport.runTestCase(testResponse, SortSolutionType.INSERTION);
+	}
+
+	@Test
+	public void testMergeSortRecursiveNaiveSorter() {
+		testSupport.runTestCase(testResponse, SortSolutionType.MERGE_RECURSIVE_SORTED_BY_INSERTION);
+	}
+
+	@Test
+	public void testMergeSortRecursiveArrayCopy() {
+		testSupport.runTestCase(testResponse, SortSolutionType.MERGE_RECURSIVE_ARRAY_COPY);
+	}
+
+	@Test
+	public void testMergeSortRecursiveNaiveNLogNSpace() {
+		testSupport.runTestCase(testResponse, SortSolutionType.MERGE_RECURSIVE_NLOGN_SPACE);
+	}
+
+	@Test
+	public void testMergeSortRecursiveNaiveNSpace() {
+		testSupport.runTestCase(testResponse, SortSolutionType.MERGE_RECURSIVE_N_SPACE);
+	}
+
+	@Test
+	public void testMergeSortRecursiveNaiveOneSpace() {
+		testSupport.runTestCase(testResponse, SortSolutionType.MERGE_RECURSIVE_ONE_SPACE);
+	}
+
+	@Test
+	public void testMergeSortRecursivePolished() {
+		testSupport.runTestCase(testResponse, SortSolutionType.MERGE_RECURSIVE_NLOGN_SPACE_DIVISION_ON_MERGE);
+	}
+
+}// End of Class
