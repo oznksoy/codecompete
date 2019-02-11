655f2fed5cb33fdc9e66c92b422cd648c9e5ed4e
ozan aksoy <rockcoden@gmail.com>
2019-02-11 18:03:23 +0300

    [ADD]: Nonrecursive mergesort solution test cases are added

---
+++ src/test/java/test/solution/sort/TestMergeSolutionNonRecursive.java
@@ -0,0 +1,69 @@
+package test.solution.sort;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.algorithms.sorting.SortSolutionType;
+
+public class TestMergeSolutionNonRecursive extends AbstSortSolutionTest {
+
+	@Before
+	public void setup() throws Exception {
+		setSolutionTypeToTest();
+	}
+
+	@Override
+	public void setSolutionTypeToTest() {
+		solutionType = SortSolutionType.MERGE_NON_RECURSIVE;
+	}
+
+	@Override
+	@Test
+	public void testRandomizedTestCase() {
+		testSupport.runRandomizedTestCase(500000, 200, 0, solutionType);
+	}
+
+	@Override
+	@Test
+	@Ignore
+	public void testBestCase() {
+		super.testBestCase();
+	}
+
+	@Override
+	@Test
+	@Ignore
+	public void testWorstCase() {
+		super.testWorstCase();
+	}
+
+	@Override
+	@Test
+	@Ignore
+	public void testHalfInOrderatRandom() {
+		super.testHalfInOrderatRandom();
+	}
+
+	@Override
+	@Test
+	@Ignore
+	public void testHalfInOrderOnMedian() {
+		super.testHalfInOrderOnMedian();
+	}
+
+	@Override
+	@Test
+	@Ignore
+	public void testHalfInOrderOnMedianInReverse() {
+		super.testHalfInOrderOnMedianInReverse();
+	}
+
+	@Override
+	@Test
+	@Ignore
+	public void testOneToFiveCase() {
+		super.testOneToFiveCase();
+	}
+
+}// End of Test Case
