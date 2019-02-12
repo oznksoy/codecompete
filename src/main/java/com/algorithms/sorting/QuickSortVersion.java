a3d93e5624fdfbb8ee58a1ba798465f48bb31e45
ozan aksoy <rockcoden@gmail.com>
2019-02-12 22:31:49 +0300

    [ADD]: Quicksort Scheme is added

---
+++ src/main/java/com/algorithms/sorting/QuickSortVersion.java
@@ -0,0 +1,8 @@
+package com.algorithms.sorting;
+
+public enum QuickSortVersion {
+	INITIAL_PIVOT, //
+	MEDIAN_PIVOT, //
+	RANDOM_PIVOT, //
+	DUAL_PIVOT;
+}
---
+++ src/main/java/com/algorithms/sorting/QuicksortSolution.java
@@ -0,0 +1,70 @@
+package com.algorithms.sorting;
+
+import com.algorithms.sorting.exception.QuickSortSortSolutionSelectionException;
+
+class QuicksortSolution {
+
+	private SortSolution activeSolution;
+	private SortSolutionUtil util;
+
+	public QuicksortSolution(QuickSortVersion version) throws QuickSortSortSolutionSelectionException {
+		util = SortSolutionUtil.getInstance();
+		activeSolution = setQuickSortSolutions(version);
+	}
+
+	public SortSolution getActiveSolution() {
+		return this.activeSolution;
+	}
+
+	public SortSolution setQuickSortSolutions(QuickSortVersion version) throws QuickSortSortSolutionSelectionException {
+		switch (version) {
+		case INITIAL_PIVOT:
+			return new QuickSortInitialPivot();
+		case MEDIAN_PIVOT:
+			return new QuickSortMiddleIndexPivot();
+		case RANDOM_PIVOT:
+			return new QuickSortRandomIndexPivot();
+		case DUAL_PIVOT:
+			return new QuickSortDualPivot();
+		default:
+			throw new QuickSortSortSolutionSelectionException();
+		}
+	}
+
+	private class QuickSortInitialPivot implements SortSolution {
+
+		@Override
+		public int[] sort(int[] inputArray) {
+			return null;
+		}
+
+	}
+
+	private class QuickSortMiddleIndexPivot implements SortSolution {
+
+		@Override
+		public int[] sort(int[] inputArray) {
+			return null;
+		}
+
+	}
+
+	private class QuickSortRandomIndexPivot implements SortSolution {
+
+		@Override
+		public int[] sort(int[] inputArray) {
+			return null;
+		}
+
+	}
+
+	private class QuickSortDualPivot implements SortSolution {
+
+		@Override
+		public int[] sort(int[] inputArray) {
+			return null;
+		}
+
+	}
+
+}
