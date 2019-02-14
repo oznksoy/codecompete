aa898394d73c5860ecdcf4ab40a675b7bc5f6b36
ozan aksoy <rockcoden@gmail.com>
2019-02-14 14:30:46 +0300

    [ADD]: Divisible Sum Pairs solution

---
+++ src/main/java/com/algorithms/hackerrank/HackerrankStandardFileReader.java
@@ -0,0 +1,49 @@
+package com.algorithms.hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class HackerrankStandardFileReader {
+
+	static int func() {
+
+		return 0;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] nk = scanner.nextLine().split(" ");
+
+		int n = Integer.parseInt(nk[0]);
+
+		int k = Integer.parseInt(nk[1]);
+
+		int[] ar = new int[n];
+
+		String[] arItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int i = 0; i < n; i++) {
+			int arItem = Integer.parseInt(arItems[i]);
+			ar[i] = arItem;
+		}
+
+		int result = func();// funtion to implement;
+
+		bufferedWriter.write(String.valueOf(result));
+		bufferedWriter.newLine();
+
+		bufferedWriter.close();
+
+		scanner.close();
+	}
+
+}
