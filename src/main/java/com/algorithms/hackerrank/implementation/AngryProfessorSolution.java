93ff8c593d066c9d8c73e473835f3a2f7b63b61e
ozan aksoy <rockcoden@gmail.com>
2019-02-17 13:07:06 +0300

    [UPDATE]: Angry Professor Solution is added

--- src/main/java/com/algorithms/hackerrank/implementation/AngryProfessorSolution.java
+++ src/main/java/com/algorithms/hackerrank/implementation/AngryProfessorSolution.java
@@ -1,14 +1,34 @@
 package com.algorithms.hackerrank.implementation;
 
+/**
+ * Discrete Mathematics professor has a class of students. Frustrated with their
+ * lack of discipline, he decides to cancel class if fewer than some number of
+ * students are present when class starts. Arrival times go from on time
+ * (arrivalTime <= 0) to arrived late ( arrivalTime > 0).
+ * 
+ * Given the arrival time of each student and a threshhold number of attendees,
+ * determine if the class is canceled.
+ * 
+ * @see <a href=
+ *      "https://www.hackerrank.com/challenges/angry-professor/problem">angry-professor</a>
+ */
 public class AngryProfessorSolution {
 
 	// Complete the angryProfessor function below.
+	// Is class cancelled?
 	static String angryProfessor(int k, int[] a) {
 
-		
-		
-		return null;
-		
+		int lateCount = 0;
+		for (int v : a) {
+			if (v > 0) {
+				lateCount++;
+			}
+			if ((a.length - lateCount) < k) {
+				return "YES";
+			}
+		}
+		return "NO";
+
 	}// End of Method
 
 	public static void main(String args[]) {
@@ -19,7 +39,9 @@ public class AngryProfessorSolution {
 	}// End of Main
 
 	static void testAngryProfessor(int k, int[] a, String expected) {
-
+		String result = angryProfessor(k, a);
+		System.out.println(result);
+		assert expected.equals(result);
 	}// End of Test
 
 }// End of Class
