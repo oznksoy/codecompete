4db471d800f378112dfb77c2cd05948129e5ca84
ozan aksoy <rockcoden@gmail.com>
2019-02-17 12:52:15 +0300

    [ADD]: AngryProfessorSolution Scheme is added

---
+++ src/main/java/com/algorithms/hackerrank/implementation/AngryProfessorSolution.java
@@ -0,0 +1,25 @@
+package com.algorithms.hackerrank.implementation;
+
+public class AngryProfessorSolution {
+
+	// Complete the angryProfessor function below.
+	static String angryProfessor(int k, int[] a) {
+
+		
+		
+		return null;
+		
+	}// End of Method
+
+	public static void main(String args[]) {
+
+		testAngryProfessor(3, new int[] { -1, -3, 4, 2 }, "YES");
+		testAngryProfessor(2, new int[] { 0, -1, 2, 1 }, "NO");
+
+	}// End of Main
+
+	static void testAngryProfessor(int k, int[] a, String expected) {
+
+	}// End of Test
+
+}// End of Class
