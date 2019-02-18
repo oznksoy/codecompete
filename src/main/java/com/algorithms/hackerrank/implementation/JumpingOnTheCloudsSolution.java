027f70f9d54b773dfe1bc8fdb7da7aacbfeb288a
ozan aksoy <rockcoden@gmail.com>
2019-02-18 23:58:20 +0300

    [ADD]: JumpingOnTheCloudsSolution Scheme was added

---
+++ src/main/java/com/algorithms/hackerrank/implementation/JumpingOnTheCloudsSolution.java
@@ -0,0 +1,22 @@
+package com.algorithms.hackerrank.implementation;
+
+public class JumpingOnTheCloudsSolution {
+
+	// Complete the jumpingOnClouds function below.
+	static int jumpingOnClouds(int[] c, int k) {
+
+		return 0;
+
+	}// End of Method
+
+	public static void main(String[] args) {
+
+	}// End of Main
+
+	static void testJumpingOnClouds(int[] c, int k, int expected) {
+		int result = jumpingOnClouds(c, k);
+		System.out.println(result);
+		assert result == expected;
+	}
+
+}// End of Class
