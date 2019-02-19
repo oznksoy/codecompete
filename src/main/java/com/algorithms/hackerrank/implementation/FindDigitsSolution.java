48ad027d143586b20117317398a71bf3855e94d5
ozan aksoy <rockcoden@gmail.com>
2019-02-19 15:57:01 +0300

    [ADD]: FindDigitsSolution was added

---
+++ src/main/java/com/algorithms/hackerrank/implementation/FindDigitsSolution.java
@@ -0,0 +1,33 @@
+package com.algorithms.hackerrank.implementation;
+
+public class FindDigitsSolution {
+
+	// Complete the findDigits function below.
+	static int findDigits(int n) {
+
+		int v = n;
+		int count = 0;
+		while (v > 0) {
+			int digit = v % 10;
+			if (digit != 0 && n % digit == 0) {
+				count++;
+			}
+			v = (v - digit) / 10;
+		}
+
+		return count;
+
+	}// End of Method
+
+	public static void main(String[] args) {
+		testFindDigits(12, 2);
+		testFindDigits(1012, 3);
+	}// End of Main
+
+	static void testFindDigits(int n, int expected) {
+		int result = findDigits(n);
+		System.out.println(result);
+		assert result == expected;
+	}// End of Test
+
+}// End of Classs
