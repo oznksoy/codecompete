0db382035d60ca908148dee4be7d1c5c9a5421fd
ozan aksoy <rockcoden@gmail.com>
2019-02-19 15:44:12 +0300

    [UPDATE]: jumpingOnClouds solution is updated to O(n) solution

--- src/main/java/com/algorithms/hackerrank/implementation/JumpingOnTheCloudsSolution.java
+++ src/main/java/com/algorithms/hackerrank/implementation/JumpingOnTheCloudsSolution.java
@@ -5,11 +5,31 @@ public class JumpingOnTheCloudsSolution {
 	// Complete the jumpingOnClouds function below.
 	static int jumpingOnClouds(int[] c, int k) {
 
-		return 0;
+		int e = 100;
+		// uses 1 energy to make a jump
+		// Length of 1 jump : k
+		// Configuration of the clouds to jump on : c
+
+		int n = c.length;
+
+		for (int i = 1;; i++) {
+			int ind = ((i * k) % n);
+			e -= 1;
+			if (1 == c[ind]) {
+				e -= 2;
+			}
+			if(i * k >= n) {
+				break;
+			}
+		}
+		
+		return e;
 
 	}// End of Method
 
 	public static void main(String[] args) {
+
+		testJumpingOnClouds(new int[] { 0, 0, 1, 0, 0, 1, 1, 0 }, 2, 92);
 
 	}// End of Main
 
