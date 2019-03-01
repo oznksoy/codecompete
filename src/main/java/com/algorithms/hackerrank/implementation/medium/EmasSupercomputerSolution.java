fe99dc44facc992de43bd330d0de356bdcc66b80
ozan aksoy <rockcoden@gmail.com>
2019-03-01 15:29:11 +0300

    [UPDATE]: Ema's supercomputer solution

--- src/main/java/com/algorithms/hackerrank/implementation/medium/EmasSupercomputerSolution.java
+++ src/main/java/com/algorithms/hackerrank/implementation/medium/EmasSupercomputerSolution.java
@@ -2,4 +2,21 @@ package com.algorithms.hackerrank.implementation.medium;
 
 public class EmasSupercomputerSolution {
 
-}
+	// Complete the twoPluses function below.
+	static int twoPluses(String[] grid) {
+
+		return 0;
+
+	}// End of Method
+
+	public static void main(String[] args) {
+
+	}// End of Main
+
+	static void testTwoPluses(String[] grid, int expected) {
+		int result = twoPluses(grid);
+		System.out.println("Result : " + result);
+		assert expected == result;
+	}// End of Test
+
+}// End of Class
