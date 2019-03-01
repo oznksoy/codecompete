52f8184aabd8c718a68170d2685876b4f205447d
ozan aksoy <rockcoden@gmail.com>
2019-03-01 14:48:27 +0300

    [UPDATE]: The bomberman solution was added.

--- src/main/java/com/algorithms/hackerrank/implementation/medium/TheBombermanGameSolution.java
+++ src/main/java/com/algorithms/hackerrank/implementation/medium/TheBombermanGameSolution.java
@@ -24,9 +24,144 @@ public class TheBombermanGameSolution {
 			return grid;
 		} else if (n == 2) { // Since n can be 2 also, then
 			return fullBombedGrid(grid[0].length(), grid.length);
+		} // 3,4,5 cycles;
+
+		// Every odd cycle is repetition of the third cycle or the fifth cycle
+		// Every even cycle is a full bombed grid
+
+		int mod = (n - 2) % 2;
+		if (mod == 1) {
+			int rep = ((n - 1) / 2) % 2;
+			if (rep == 1) { // repetition of the third cycle
+				return fillThirdCycle(grid);
+			} else { // repetition of the fifth cycle
+				return fillFifthCycle(grid);
+			}
+		} else { // (mod == 0)
+			return fullBombedGrid(grid[0].length(), grid.length);
 		}
 
-		return null;
+	}// End of Method
+
+	/**
+	 * Reverse the third cycle state
+	 * 
+	 * @param grid : playground
+	 * @return fifth state
+	 */
+	static String[] fillFifthCycle(String[] grid) {
+		String[] fifthCycle = new String[grid.length];
+		String[] thridCycle = fillThirdCycle(grid);
+
+		int rows = thridCycle[0].length();
+		int columns = thridCycle.length;
+		for (int c = 0; c < columns; c++) {
+			String level = "";
+			for (int r = 0; r < rows; r++) {
+				if (isThisAnImpactZone(r, c, thridCycle, rows, columns)) {
+					level = level.concat(".");
+				} else {
+					level = level.concat("O");
+				}
+			}
+			fifthCycle[c] = level;
+		}
+
+		return fifthCycle;
+	}// End of Method
+
+	/**
+	 * Fill third second's state as third cycle and fill the grid
+	 * 
+	 * @param grid
+	 * @return third second's state
+	 */
+	static String[] fillThirdCycle(String[] grid) {
+		String[] thridCycle = new String[grid.length];
+		int rows = grid[0].length();
+		int columns = grid.length;
+		for (int c = 0; c < columns; c++) {
+			String level = "";
+			for (int r = 0; r < rows; r++) {
+				if (isThisAnImpactZone(r, c, grid, rows, columns)) {
+					level = level.concat(".");
+				} else {
+					level = level.concat("O");
+				}
+			}
+			thridCycle[c] = level;
+		}
+		return thridCycle;
+	}// End of Method
+
+	/**
+	 * Turns true if there is bomb nearby
+	 * 
+	 * @param r       : row index
+	 * @param c       : column index
+	 * @param grid    : playground
+	 * @param rows    : total number of rows
+	 * @param columns : total number of columns
+	 * @return
+	 */
+	static boolean isThisAnImpactZone(int r, int c, String[] grid, int rows, int columns) {
+
+		// check self
+
+		if ('O' == grid[c].charAt(r)) {
+			return true;
+		}
+
+		// check up
+		char up;
+		if (c > 0) {
+			up = grid[c - 1].charAt(r);
+		} else {
+			up = '.';
+		}
+
+		if (up == 'O') {
+			return true;
+		}
+
+		// check down
+		char down;
+		if (c < columns - 1) {
+			down = grid[c + 1].charAt(r);
+		} else {
+			down = '.';
+		}
+
+		if (down == 'O') {
+			return true;
+		}
+
+		// check left
+		char left;
+		if (r > 0) {
+			left = grid[c].charAt(r - 1);
+		} else {
+			left = '.';
+		}
+
+		if (left == 'O') {
+			return true;
+		}
+
+		// check left
+		char right;
+		if (r < rows - 1) {
+			right = grid[c].charAt(r + 1);
+		} else {
+			right = '.';
+		}
+
+		if (right == 'O') {
+			return true;
+		}
+
+		return false;
+
 	}// End of Method
 
 	static String[] fullBombedGrid(int row, int column) {
@@ -40,7 +175,7 @@ public class TheBombermanGameSolution {
 			bombedGrid[c] = s;
 		}
 		return bombedGrid;
-	}
+	}// End of Method
 
 	public static void main(String[] args) {
 
@@ -51,6 +186,15 @@ public class TheBombermanGameSolution {
 		testBomberMan(2, //
 				new String[] { "...O", "...O", "...O", "...O" }, //
 				new String[] { "OOOO", "OOOO", "OOOO", "OOOO" } //
+		);
+
+		testBomberMan(3, //
+				new String[] { ".......", "...O...", "....O..", ".......", "OO.....", "OO....." }, //
+				new String[] { "OOO.OOO", "OO...OO", "OOO...O", "..OO.OO", "...OOOO", "...OOOO" } //
+		);
+		testBomberMan(5, //
+				new String[] { ".......", "...O.O.", "....O..", "..O....", "OO...OO", "OO.O..." }, //
+				new String[] { ".......", "...O.O.", "...OO..", "..OOOO.", "OOOOOOO", "OOOOOOO" } //
 		);
 
 	}// End of Main
