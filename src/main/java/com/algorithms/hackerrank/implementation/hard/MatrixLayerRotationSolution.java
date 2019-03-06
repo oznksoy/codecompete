0e460e5516f4722a8bc3e316d407f28507d06d6a
ozan aksoy <rockcoden@gmail.com>
2019-03-06 12:17:58 +0300

    [UPDATE]: MatrixLayer Rotation Test Update

--- src/main/java/com/algorithms/hackerrank/implementation/hard/MatrixLayerRotationSolution.java
+++ src/main/java/com/algorithms/hackerrank/implementation/hard/MatrixLayerRotationSolution.java
@@ -52,8 +52,56 @@ public class MatrixLayerRotationSolution {
 				});
 			}
 		}, 2);
-		
-		
+
+		// 28 27 26 25
+		// 22 9 15 19
+		// 16 8 21 13
+		// 10 14 20 7
+		// 4 3 2 1
+		testMatrixRotation(new ArrayList<List<Integer>>() {
+			{
+				add(new ArrayList<Integer>() {
+					{
+						add(1);
+						add(2);
+						add(3);
+						add(4);
+					}
+				});
+				add(new ArrayList<Integer>() {
+					{
+						add(7);
+						add(8);
+						add(9);
+						add(10);
+					}
+				});
+				add(new ArrayList<Integer>() {
+					{
+						add(13);
+						add(14);
+						add(15);
+						add(16);
+					}
+				});
+				add(new ArrayList<Integer>() {
+					{
+						add(19);
+						add(20);
+						add(21);
+						add(22);
+					}
+				});
+				add(new ArrayList<Integer>() {
+					{
+						add(25);
+						add(26);
+						add(27);
+						add(28);
+					}
+				});
+			}
+		}, 7);
 
 	}// End of Main
 
