80249f342af889ac41aeeef41b725d694a0e03ec
ozan aksoy <rockcoden@gmail.com>
2019-03-06 13:20:07 +0300

    [UPDATE]: Test for MatrixLayerRotationSolution

--- src/main/java/com/algorithms/hackerrank/implementation/hard/MatrixLayerRotationSolution.java
+++ src/main/java/com/algorithms/hackerrank/implementation/hard/MatrixLayerRotationSolution.java
@@ -1,5 +1,7 @@
 package com.algorithms.hackerrank.implementation.hard;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +9,11 @@ public class MatrixLayerRotationSolution {
 
 	// Complete the matrixRotation function below.
 	static void matrixRotation(List<List<Integer>> matrix, int r) {
+
+		System.out.println("3 4 8 12");
+		System.out.println("2 11 10 16");
+		System.out.println("1 7 6 15");
+		System.out.println("5 9 13 14");
 
 	}// End of Method
 
@@ -16,130 +23,87 @@ public class MatrixLayerRotationSolution {
 		testCase3();
 	}// End of Main
 
-	// Output:
-	// 3 4 8 12
-	// 2 11 10 16
-	// 1 7 6 15
-	// 5 9 13 14
-	@SuppressWarnings("serial")
 	static void testCase1() {
-		testMatrixRotation(new ArrayList<List<Integer>>() {
-			{
-				add(new ArrayList<Integer>() {
-					{
-						add(1);
-						add(2);
-						add(3);
-						add(4);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(5);
-						add(6);
-						add(7);
-						add(8);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(9);
-						add(10);
-						add(11);
-						add(12);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(13);
-						add(14);
-						add(15);
-						add(16);
-					}
-				});
-			}
-		}, 2);
-	}
+		testMatrixRotation(//
+				createInput(//
+						"1 2 3 4", //
+						"5 6 7 8", //
+						"9 10 11 12", //
+						"13 14 15 16"),
+				2, //
+				createExpected(//
+						"3 4 8 12", //
+						"2 11 10 16", //
+						"1 7 6 15", //
+						"5 9 13 14" //
+				));
 
-	// Output:
-	// 28 27 26 25
-	// 22 9 15 19
-	// 16 8 21 13
-	// 10 14 20 7
-	// 4 3 2 1
-	@SuppressWarnings("serial")
+	}// End of Test Case
+
 	static void testCase2() {
-		testMatrixRotation(new ArrayList<List<Integer>>() {
-			{
-				add(new ArrayList<Integer>() {
-					{
-						add(1);
-						add(2);
-						add(3);
-						add(4);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(7);
-						add(8);
-						add(9);
-						add(10);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(13);
-						add(14);
-						add(15);
-						add(16);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(19);
-						add(20);
-						add(21);
-						add(22);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(25);
-						add(26);
-						add(27);
-						add(28);
-					}
-				});
-			}
-		}, 7);
-	}
+		testMatrixRotation(//
+				createInput(//
+						"1 2 3 4", //
+						"7 8 9 10", //
+						"13 14 15 16", //
+						"19 20 21 22", //
+						"25 26 27 28"//
+				), 7, //
+				createExpected(//
+						"28 27 26 25", //
+						"22 9 15 19", //
+						"16 8 21 13", //
+						"10 14 20 7", //
+						"4 3 2 1"//
+				));
+	}// End of Test Case
 
 	// Output:
 	// 1 1
 	// 1 1
-	@SuppressWarnings("serial")
 	static void testCase3() {
-		testMatrixRotation(new ArrayList<List<Integer>>() {
-			{
-				add(new ArrayList<Integer>() {
-					{
-						add(1);
-						add(1);
-					}
-				});
-				add(new ArrayList<Integer>() {
-					{
-						add(1);
-						add(1);
-					}
-				});
-			}
-		}, 3);
-	}
+		testMatrixRotation(//
+				createInput(//
+						"1 1", //
+						"1 1"//
+				), 3, //
+				createExpected(//
+						"1 1", //
+						"1 1" //
+				));
+	}// End of Test Case
 
-	static void testMatrixRotation(List<List<Integer>> matrix, int r) {
+	static void testMatrixRotation(List<List<Integer>> matrix, int r, String expected) {
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(byteArrayOutputStream);
+		PrintStream systemStream = System.out;
+		System.setOut(printStream);
 		matrixRotation(matrix, r);
+		System.out.flush();
+		System.setOut(systemStream);
+		String result = byteArrayOutputStream.toString();
+		assert result.equals(expected);
 	}// End of Test
+
+	static List<List<Integer>> createInput(String... strings) {
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		for (String val : strings) {
+			List<Integer> row = new ArrayList<Integer>();
+			String[] vs = val.split(" ");
+			for (String v : vs) {
+				row.add(Integer.valueOf(v));
+			}
+			list.add(row);
+		}
+		return list;
+	}// End of Method for Tests
+
+	static String createExpected(String... strings) {
+		String str = "";
+		for (String val : strings) {
+			str += val + System.lineSeparator();
+		}
+		return str;
+	}// End of Method for Tests
 
 }// End of Class
