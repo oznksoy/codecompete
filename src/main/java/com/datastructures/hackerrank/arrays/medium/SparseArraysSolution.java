0022eabd184437ab870460145d1f282e93c6504a
ozan aksoy <rockcoden@gmail.com>
2019-03-07 23:56:40 +0300

    [ADD]: SparseArraysSolution is added

--- src/main/java/com/datastructures/hackerrank/arrays/medium/SparseArraysSolution.java
+++ src/main/java/com/datastructures/hackerrank/arrays/medium/SparseArraysSolution.java
@@ -1,11 +1,27 @@
 package com.datastructures.hackerrank.arrays.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SparseArraysSolution {
 
 	// Complete the matchingStrings function below.
 	static int[] matchingStrings(String[] strings, String[] queries) {
 
-		return null;
+		Map<String, Integer> inputMap = new HashMap<>();
+
+		for (String key : strings) {
+			inputMap.put(key, inputMap.get(key) == null ? 1 : inputMap.get(key) + 1);
+		}
+
+		int[] values = new int[queries.length];
+
+		for (int i = 0; i < queries.length; i++) {
+			values[i] = inputMap.get(queries[i]) == null ? 0 : inputMap.get(queries[i]);
+		}
+
+		return values;
+
 	}// End of Method
 
 	public static void main(String[] args) {
